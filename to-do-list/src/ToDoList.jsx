7ad7c031d6d52c125462b19df58e22c12e00924e
olusegun <olusegunbamgbelu755@gmail.com>
2024-09-19 15:50:46 +0100

    to-do list component ongoing

---
+++ to-do-list/src/ToDoList.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+function ToDoList() {
+    const [tasks, setTasks] = useState(["Study the bible", "Pray", "Shower"]); 
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event) {
+        setNewTask(event.target.value)
+    }
+
+    function addTask() {
+
+    }
+
+    function deleteTask(index) {
+
+    }
+
+    function moveTaskUp(index) {
+
+    }
+
+    function moveTaskDown(index) {
+
+    }
+
+    return(
+        <div className='to-do-list'>
+
+            <h1>Kratos To-Do-List</h1>
+
+            <div>
+                <input 
+                    type="text" 
+                    placeholder='Enter a task...' value={newTask} 
+                    onChange={handleInputChange} />
+                <button 
+                className='add-button'
+                onClick={addTask}>
+                    Add
+                </button>
+            </div>
+
+            <ol>
+                {tasks.map((task, index) =>
+                <li key={index}>
+                    <span className="text">{task}</span>
+                    <button 
+                        className="delete-btn"
+                        onClick={() => deleteTask(index)}>
+                        Delete
+                    </button>
+                </li>)}
+            </ol>
+        </div>
+    );
+}
+
+export default ToDoList;
