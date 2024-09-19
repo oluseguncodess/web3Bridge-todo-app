e13e27d0ac641ae14f1b8fbc81e7fe8d20c0f637
olusegun <olusegunbamgbelu755@gmail.com>
2024-09-19 18:01:20 +0100

    to-do app complete

--- to-do-list/src/ToDoList.jsx
+++ to-do-list/src/ToDoList.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 function ToDoList() {
-    const [tasks, setTasks] = useState(["Study the bible", "Pray", "Shower"]); 
+    const [tasks, setTasks] = useState([]); 
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event) {
@@ -10,18 +10,33 @@ function ToDoList() {
 
     function addTask() {
 
+        if(newTask.trim() != "") {
+            setTasks(t => [...t, newTask]);
+            setNewTask("");
+        }
+        
     }
 
     function deleteTask(index) {
-
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
     }
 
     function moveTaskUp(index) {
-
+        if(index > 0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
     function moveTaskDown(index) {
-
+         
+        if (index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
     return(
@@ -45,6 +60,7 @@ function ToDoList() {
                 {tasks.map((task, index) =>
                 <li key={index}>
                     <span className="text">{task}</span>
+                    <div className="btn-container">
                     <button 
                         className="delete-btn"
                         onClick={() => deleteTask(index)}>
@@ -60,6 +76,8 @@ function ToDoList() {
                         onClick={() => moveTaskDown(index)}>
                         ⬇
                     </button>
+                    </div>
+                    
                 </li>)}
             </ol>
         </div>
